988283691cc3b63a42e4fcbd3b8ae1f667c43343
IrinaSing <irinasingh.mail@yandex.ru>
2021-08-10 19:59:34 +0200

    schema

--- client/data/quiz.schema.js
+++ client/data/quiz.schema.js
@@ -1,79 +1,30 @@
 export const schema = {
-  title: 'quiz data',
-  description:
-    'the questions, answers and study references for a JavaScript quiz',
-  type: 'object',
-  required: ['questions'],
+  title: "portfolio data",
+  description: "infomation to use in section About",
+  type: "object",
 
   properties: {
     greeting: {
-      type: 'string',
-      description: 'a friendly greeting for the user',
+      type: "string",
+      description: "a friendly greeting for the user",
     },
 
-    questions: {
-      type: 'array',
-      description: 'the array of questions in the quiz',
+    about: {
+      type: "strings",
+      description: "text for <p> in section about",
+    },
 
-      items: {
-        type: 'object',
-        description: 'a single question in the quiz',
-        required: ['question', 'answered', 'answers'],
-
-        properties: {
-          question: {
-            type: 'string',
-            description: 'the question a user needs to answer',
-          },
-          answered: {
-            type: 'boolean',
-            description: 'whether or not the user has answered this question',
-          },
-          answers: {
-            type: 'array',
-            description: 'the possible answers for this question',
-
-            items: {
-              type: 'object',
-              required: ['text', 'correct', 'selected'],
-
-              properties: {
-                text: {
-                  type: 'string',
-                  description: 'the text a user sees for this answer',
-                },
-                correct: {
-                  type: 'boolean',
-                  description: 'whether or not this is a correct answer',
-                },
-                selected: {
-                  type: 'boolean',
-                  description: 'has the user selected this answer',
-                },
-              },
-            },
-          },
-
-          links: {
-            type: 'array',
-            items: {
-              type: 'object',
-              required: ['url'],
-
-              properties: {
-                url: {
-                  type: 'string',
-                  description:
-                    'the url a student can open to learn more about this question',
-                },
-                text: {
-                  type: 'string',
-                  description:
-                    'the text to display for this question. if missing, the url is displayed',
-                },
-              },
-            },
-          },
+    skills: {
+      type: "array",
+      description: "info for section Skills",
+      properties: {
+        header: {
+          type: "string",
+          description: "text to render in the header of subsection",
+        },
+        text: {
+          type: "string",
+          description: "text to render in the <p> of subsection",
         },
       },
     },
