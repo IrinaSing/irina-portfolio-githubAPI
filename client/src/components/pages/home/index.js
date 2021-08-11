ff67e7a9fb1005a689f6100b6f963e7a3f959863
IrinaSing <irinasingh.mail@yandex.ru>
2021-08-11 17:47:07 +0200

    api-call, avatar element

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -1,3 +1,6 @@
+import { getApiData } from "../../../api-calls/get-data.js";
+import { createAvatar } from "./create-avatar.js";
+
 /**
  * The home page.
  *
@@ -7,6 +10,11 @@ export const home = () => {
   const container = document.createElement("div");
   container.className = "body";
   container.innerHTML = "home: ";
+
+  getApiData("https://api.github.com/users/IrinaSing").then((data) => {
+    createAvatar(data);
+  });
+  container.appendChild(createAvatar());
 
   return container;
 };
