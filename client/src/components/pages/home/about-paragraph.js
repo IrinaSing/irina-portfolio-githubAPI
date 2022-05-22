36d4d0b5854e93bfb192aa493db963e3f36d7194
Irina Singh <75549506+IrinaSing@users.noreply.github.com>
2022-05-22 17:25:41 +0200

    Update about-paragraph.js

--- client/src/components/pages/home/about-paragraph.js
+++ client/src/components/pages/home/about-paragraph.js
@@ -9,7 +9,7 @@ export const aboutParagraph = (profileData) => {
   const header = createHeader(0);
 
   // eslint-disable-next-line spellcheck/spell-checker
-  const aboutP = `My name is Irina. I’m a junior web developer located in ${getLocation}. I am passionate about creating maximally user-friendly websites and applications. My goal is to make products that help companies and users in reaching their goals.`;
+  const aboutP = `My name is Irina. I’m a junior web developer and data analyst located in ${getLocation}. I am passionate about creating user-friendly websites and applications and looking forward to join interesting projects`;
 
   const paragraph = document.createElement("p");
   const paragraphTextNode = document.createTextNode(aboutP);
